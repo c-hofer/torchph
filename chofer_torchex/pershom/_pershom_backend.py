b2fc644fd707bd00d59bb3bfc90b51da17f385d5
cdhofer <chofer@cosy.sbg.ac.at>
2018-06-15 13:09:18 +0200

    added documentation and exposed backend functiosn in python module

---
+++ chofer_torchex/pershom/_pershom_backend.py
@@ -0,0 +1,90 @@
+import os.path as pth
+from torch import Tensor
+r"""README
+
+descending_sorted_boundary array: 
+    Boundary array which encodes the boundary matrix (BM) for a given filtration in 
+    column first order.
+    Let BA be the descending_sorted_boundary of BM.
+
+        BA[i, :] -> i-th column of BM. Content encoded as decreasingly sorted list, 
+                    embedded into the array with -1 padding from the right. 
+
+        example :
+            BA[3, :] = [2, 0, -1, -1] -> boundary(v_3) = v_0 + v_2
+            BA[6, :] = [5, 4, 3, -1]  -> boundary(v_6) = v_3 + v_4 + v_5
+"""
+
+
+from torch.utils.cpp_extension import load
+
+
+__module_file_dir = pth.dirname(pth.realpath(__file__))
+__cpp_src_dir = pth.join(__module_file_dir, 'pershom_cpp_src')
+
+
+__C = load(
+    'pershom_cuda_ext',
+    [pth.join(__cpp_src_dir, 'pershom.cpp'),
+     pth.join(__cpp_src_dir, 'pershom_cuda.cu')],
+    verbose=True)
+
+
+def find_merge_pairings(pivots: Tensor, max_pairs: int)->Tensor:
+    r"""Finds the pairs which have to be merged in the current iteration. 
+    For 
+    
+    Arguments:
+        pivots {Tensor} -- [Nx1: N is the number of columns of the under
+        lying descending sorted boundary array]
+
+        max_pairs {int} -- [The output is at most a max_pairs x 2 Tensor]
+    
+    Returns:
+        Tensor -- [Nx2: N is the number of merge pairs]
+    """
+    return __C.find_merge_pairings(Tensor, max_pairs)
+
+
+def merge_columns_(
+    descending_sorted_boundary_array: Tensor, 
+    merge_pairs: Tensor
+    )->None:
+    r"""Executes the given merging operations inplace on the descending 
+    sorted boundary array. 
+    
+    Arguments:
+        descending_sorted_boundary_array {Tensor} -- [see readme section top]
+
+        merge_pairs {Tensor} -- [output of a 'find_merge_pairings' call]
+    
+    Returns:
+        None -- []
+    """
+
+    __C.merge_columns_(descending_sorted_boundary_array, merge_pairs)
+
+
+def read_barcodes(
+    pivots: Tensor, 
+    column_dimension: 
+    Tensor, max_dimension: int
+    )->[[Tensor], [Tensor]]:
+    """Reads the barcodes using the pivot of a reduced boundary array
+    
+    Arguments:
+        pivots {Tensor} -- [pivots is the first column of a 
+        descending_sorted_boundary_array]
+
+        column_dimension {Tensor} -- [Vector whose i-th entry is 
+        the dimension if the i-th vertex in the given filtration]
+
+        max_dimension {int} -- [dimension of the filtrated simplicial complex]
+    
+    Returns:
+        [[Tensor], [Tensor]] -- [ret[0][i] = non essential barcodes of dimension i
+                                 ret[1][i] = birth-times of essential classes]
+    """
+
+
+    __C.read_barcodes(pivots, column_dimension, max_dimension)
--- chofer_torchex/pershom/pershom_cpp_src/pershom.cpp
+++ chofer_torchex/pershom/pershom_cpp_src/pershom.cpp
@@ -2,13 +2,12 @@
 
 using namespace at;
 
-
 // CUDA forward declarations
 
 Tensor find_merge_pairings_cuda(Tensor, int);
 Tensor merge_columns_cuda(Tensor, Tensor);
-std::vector<std::vector<Tensor> > read_barcodes_cuda(Tensor, Tensor, int);
-std::vector<std::vector<Tensor> > calculate_persistence_cuda(Tensor, Tensor, int, int);
+std::vector<std::vector<Tensor>> read_barcodes_cuda(Tensor, Tensor, int);
+std::vector<std::vector<Tensor>> calculate_persistence_cuda(Tensor, Tensor, int, int);
 
 // C++ interface
 
@@ -18,46 +17,56 @@ std::vector<std::vector<Tensor> > calculate_persistence_cuda(Tensor, Tensor, int
   CHECK_CUDA(x);       \
   CHECK_CONTIGUOUS(x)
 
-
 Tensor find_merge_pairings(
     Tensor pivots,
-    int max_pairs){
+    int max_pairs)
+{
   CHECK_INPUT(pivots);
   assert(pivots.type().scalarType() == ScalarType::Int);
 
   return find_merge_pairings_cuda(pivots, max_pairs);
 }
 
-
-void merge_columns(
+void merge_columns_(
     Tensor descending_sorted_boundary_array,
-    Tensor merge_pairs);
+    Tensor merge_pairs)
+{
+  CHECK_INPUT(descending_sorted_boundary_array);
+  assert(descending_sorted_boundary_array.type().scalarType() == ScalarType::Int);
+  CHECK_INPUT(merge_pairs);
+  assert(merge_pairs.type().scalarType() == ScalarType::Int);
 
+  merge_columns_cuda(descending_sorted_boundary_array, merge_pairs);
+}
 
-std::vector<std::vector<Tensor> > read_barcodes(
-    Tensor pivots, 
+std::vector<std::vector<Tensor>> read_barcodes(
+    Tensor pivots,
     Tensor column_dimension,
-    int max_dimension){
-      CHECK_INPUT(pivots);
-      assert(pivots.type().scalarType() == ScalarType::Int);
-      CHECK_INPUT(column_dimension);
-      assert(column_dimension.type().scalarType() == ScalarType::Int);
-      return read_barcodes_cuda(pivots, column_dimension, max_dimension);
-    }
+    int max_dimension)
+{
 
+  CHECK_INPUT(pivots);
+  assert(pivots.type().scalarType() == ScalarType::Int);
+  CHECK_INPUT(column_dimension);
+  assert(column_dimension.type().scalarType() == ScalarType::Int);
+  return read_barcodes_cuda(pivots, column_dimension, max_dimension);
+}
 
-std::vector<std::vector<Tensor> > calculate_persistence(
+std::vector<std::vector<Tensor>> calculate_persistence(
     Tensor descending_sorted_boundary_array,
     Tensor column_dimension,
     int max_pairs,
     int max_dimension)
 {
+
   CHECK_INPUT(descending_sorted_boundary_array);
   assert(descending_sorted_boundary_array.type().scalarType() == ScalarType::Int);
+  CHECK_INPUT(column_dimension);
+  assert(column_dimension.type().scalarType() == ScalarType::Int);
 
   auto dgms = calculate_persistence_cuda(descending_sorted_boundary_array,
                                          column_dimension,
-                                         max_pairs, 
+                                         max_pairs,
                                          max_dimension);
   return dgms;
 }
@@ -65,16 +74,17 @@ std::vector<std::vector<Tensor> > calculate_persistence(
 //-------------devel
 Tensor my_test_f_cuda(Tensor);
 
-Tensor my_test_f(Tensor t){
+Tensor my_test_f(Tensor t)
+{
   return my_test_f_cuda(t);
 }
 //-------------
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-  m.def("_find_merge_pairings", &find_merge_pairings, "find_merge_pairings (CUDA)");
-  // m.def("_merge_columns", &merge_columns, "merge_columns (CUDA)");
-  m.def("_read_barcodes", &read_barcodes, "read_barcodes (CUDA)");
+  m.def("find_merge_pairings", &find_merge_pairings, "find_merge_pairings (CUDA)");
+  m.def("merge_columns_", &merge_columns_, "merge_columns (CUDA)");
+  m.def("read_barcodes", &read_barcodes, "read_barcodes (CUDA)");
   m.def("calculate_persistence", &calculate_persistence, "calculate_persistence (CUDA)");
   m.def("my_test_f", &my_test_f, "test function");
 }
