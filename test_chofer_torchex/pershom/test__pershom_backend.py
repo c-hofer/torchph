e4c690b2618daa3a8ac63f1cd339097536e80d59
cdhofer <chofer@cosy.sbg.ac.at>
2018-06-15 14:09:24 +0200

    preparation for starting with unittests

---
+++ test_chofer_torchex/pershom/test__pershom_backend.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+import chofer_torchex.pershom._pershom_backend as __C
+
+class MyTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
