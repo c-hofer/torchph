c3d7ab7334933ef2b34060e8b0414f5d2baa5a1e
cdhofer <chofer@cosy.sbg.ac.at>
2018-06-19 12:03:29 +0200

    ADD: python implementation of calculate_persistence which uses backend functions. Used for profiling

---
+++ chofer_torchex/pershom/calculate_persistence.py
@@ -0,0 +1,32 @@
+from ._pershom_backend import find_merge_pairings, merge_columns_, read_barcodes
+import torch 
+from torch import Tensor
+
+def calculate_persistence(
+    descending_sorted_boundary_array: Tensor,
+    column_dimension: Tensor,
+    max_pairs: int,
+    max_dimension: int
+    )->[Tensor]:
+
+    iterations = 0
+    pivots = None
+    while True:
+        pivots = descending_sorted_boundary_array[:, 0].unsqueeze(1).contiguous()
+
+        try:
+            merge_pairings = find_merge_pairings(pivots, max_pairs)
+
+        except Exception as ex:
+            print(ex)
+
+            print("Reached end of reduction after {} iterations".format(iterations))
+            break
+
+        merge_columns_(descending_sorted_boundary_array, merge_pairings)
+        iterations += 1
+
+    barcodes = read_barcodes(pivots, column_dimension, max_dimension)
+
+    return barcodes 
+        
