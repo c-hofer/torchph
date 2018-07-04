d76b9bc4c8c5ef9d28ee32f52d645c0c2d91dc9f
cdhofer <chofer@cosy.sbg.ac.at>
2018-07-04 08:23:15 +0200

    extracted cuda parameter checks to cuh file

---
+++ chofer_torchex/pershom/pershom_cpp_src/param_checks_cuda.cuh
@@ -0,0 +1,7 @@
+#include <ATen/ATen.h>
+
+#define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) \
+  CHECK_CUDA(x);       \
+  CHECK_CONTIGUOUS(x)
