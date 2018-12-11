e085b62f415c4999cca7df21372f28a54f1f7a09
cdhofer <chofer@cosy.sbg.ac.at>
2018-12-11 07:46:12 +0100

    FIX: reference to _TensorBase exchanged with Tensor

--- chofer_torchex/utils/functional.py
+++ chofer_torchex/utils/functional.py
@@ -19,5 +19,5 @@ def collection_cascade(input, stop_predicate: callable, function_to_apply: calla
 
 def cuda_cascade(input, **kwargs):
     return collection_cascade(input,
-                              stop_predicate=lambda x: isinstance(x, torch._TensorBase),
+                              stop_predicate=lambda x: isinstance(x, torch.Tensor),
                               function_to_apply=lambda x: x.cuda(**kwargs))
